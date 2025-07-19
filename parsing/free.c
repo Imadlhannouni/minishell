2d2557201086e423f1ec3b5c32239dafbef59af8
Imadlhannouni <lhannouni05@gmail.com>
2025-07-19 21:28:46 +0100

    leak dnem

--- parsing/free.c
+++ parsing/free.c
@@ -6,7 +6,7 @@
 /*   By: ilhannou <ilhannou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 15:11:46 by ilhannou          #+#    #+#             */
-/*   Updated: 2025/07/18 21:37:59 by ilhannou         ###   ########.fr       */
+/*   Updated: 2025/07/19 21:22:09 by ilhannou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,14 @@ void	free_tokens(t_token *tokens)
 }
 
 
-void	free_pipes(t_pipe *pipes)
+void	free_pipes(t_pipe **pipes)
 {
     t_pipe	*tmp;
 
-    while (pipes)
+    while (*pipes)
     {
-        tmp = pipes;
-        pipes = (pipes)->nextpipe;
+        tmp = *pipes;
+        *pipes = (*pipes)->nextpipe;
         free_tokens(tmp->full_cmd);
         free(tmp);
     }
