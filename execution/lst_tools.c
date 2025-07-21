6da3911e3d603f884444cac3fd43a8a7a638bb1a
Abderrahmane Benmoussa <abderrahmanebenmoussa79@gmail.com>
2025-07-21 20:34:34 +0100

    hel

--- execution/lst_tools.c
+++ execution/lst_tools.c
@@ -53,3 +53,57 @@ void	free_t_exe(t_exe **var)
 		free(tmp);
 	}
 }
+
+int	fill_redirection(t_exe *var, t_token *tok)
+{
+	if (!(tok->heredoc != 0 || tok->inp_red != 0
+		|| tok->out_app != 0 || tok->out_red != 0))
+		return 1;
+	if (tok->heredoc != 0 || tok->inp_red != 0)
+	{
+		var->in_red_type = 1;
+		if (var->in_red_file)
+			free(var->in_red_file);
+		var->in_red_file = ft_strdup(tok->value);
+		if (!var->in_red_file)
+			return 0;
+	}
+	else if (tok->out_app != 0 || tok->out_red)
+	{
+		if (tok->out_app != 0)
+			var->out_red_type = 1;
+		else if (tok->out_red != 0)
+			var->out_red_type = 2;
+		if (var->out_red_file)
+			free(var->out_red_file);
+		var->out_red_file = ft_strdup(tok->value);
+		if (!var->out_red_file)
+			return 0;
+	}
+	return 1;
+}
+
+int	group_2d_arr(t_exe *var,t_token *tok)
+{
+	t_token	*temp;
+	int i;
+
+	i = 0;
+	temp = tok;
+	(var)->arr = malloc((count_args(tok) + 1) * sizeof(char*));
+	if (!(var)->arr)
+		return 0;
+	while (temp)
+	{
+		if ((temp->inp_red == 0) && (temp->heredoc == 0)
+			&& (temp->out_red == 0) && (temp->out_app == 0))
+			(var)->arr[i++] = ft_strdup(temp->value);
+		if (!(var)->arr[i - 1])
+			return (free_arr((var)->arr, i - 2), 0);
+		if(!fill_redirection(var, temp))
+			return (free_2d_arr((var)->arr), 0);
+		temp = temp->next;
+	}
+	(var)->arr[i] = NULL;
+	return 1;
+}
