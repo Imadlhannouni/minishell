b8646ccda23b1f314549ea1d50b3396e94c988d6
Imadlhannouni <lhannouni05@gmail.com>
2025-08-07 12:18:09 +0100

    fix

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ilhannou <ilhannou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 16:50:19 by ilhannou          #+#    #+#             */
-/*   Updated: 2025/08/06 21:57:05 by ilhannou         ###   ########.fr       */
+/*   Updated: 2025/08/07 12:16:40 by ilhannou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,28 +87,6 @@ int	handle_heredocs(t_pipe *pipe, char **clone_envi, char **exit_code)
 	return (1);
 }
 
-void	print_pipes(t_pipe *pipes)
-{
-	t_pipe	*curr_pipe;
-	t_token	*curr_token;
-
-	curr_pipe = pipes;
-	while (curr_pipe)
-	{
-		printf("=== New Pipe Command ===\n");
-		curr_token = curr_pipe->full_cmd;
-		printf("ambigious: %d\n", curr_pipe->ambigious);
-		while (curr_token)
-		{
-			printf("%s : %d : %d | ambigious : %d, inp_red : %d, out_red : %d,  heredoc : %d, out_app : %d\n", curr_token->value, curr_token->type,
-				curr_token->is_fullstring, curr_token->ambigious ,curr_token->inp_red,
-				curr_token->out_red, curr_token->heredoc, curr_token->out_app);
-			curr_token = curr_token->next;
-		}
-		curr_pipe = curr_pipe->nextpipe;
-	}
-}
-
 int	main_parsing(char *line, char **clone_envi, t_pipe **pipes,
 		char **exit_code)
 {
@@ -130,6 +108,5 @@ int	main_parsing(char *line, char **clone_envi, t_pipe **pipes,
 		compact_fullstrings(&(curr)->full_cmd);
 		curr = curr->nextpipe;
 	}
-	print_pipes(*pipes);
 	return (1);
 }
