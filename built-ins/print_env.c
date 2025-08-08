da359fb9b34a90edfc8d558d97e315e42d0e225b
Abderrahmane Benmoussa <abbenmou@c1r5p5.1337.ma>
2025-08-08 16:23:41 +0100

    sss

--- built-ins/print_env.c
+++ built-ins/print_env.c
@@ -6,7 +6,7 @@
 /*   By: abbenmou <abbenmou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 22:51:05 by abbenmou          #+#    #+#             */
-/*   Updated: 2025/08/07 14:13:50 by abbenmou         ###   ########.fr       */
+/*   Updated: 2025/08/08 16:16:48 by abbenmou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ int	print_env(char **env, char **args)
 	{
 		if (!check_equ(env[i]))
 		{
-			putstr_fd(env[i], 1);
-			putstr_fd("\n", 1);
+			printf("%s\n", env[i]);
 		}
 		i++;
 	}
